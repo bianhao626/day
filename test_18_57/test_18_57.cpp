0b88072f4c367898ba3abd9dec33c5c9382468cc
“bianhao626” <1004234098@qq.com>
2023-06-12 23:09:37 +0800

    test

---
+++ test_18_57/test_18_57.cpp
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//int main() 
+//{
+//	printf("     **     \n");
+//	printf("     **     \n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("    *  *    \n");
+//	printf("    *  *    \n");
+//	return 0;
+//}
+
+//注释多行代码ctrl+k、ctrl+c
+//取消多行注释ctrl+k、ctrl+u
+
+//#include <stdio.h>
+//
+//int main() 
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	if (a >= 140)
+//	{
+//		printf("Genius");
+//	}
+//	else
+//	{
+//		printf("No Genius");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("I lost my cellphone!");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("Name    Age    Gender\n");
+//	printf("---------------------\n");
+//	printf("Jack    18     man\n");
+//	return 0;
+//}
+
+#include <stdio.h>
+
+int main()
+{
+	int a = 40;
+	int c = 212;
+	printf("%d", (-8 + 22)*a - 10 + c / 2);
+	return 0;
+}
