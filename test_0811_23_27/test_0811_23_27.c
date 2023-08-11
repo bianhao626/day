f465693b31ca0d97bc56a4e9ee393d1cf00ad1cb
bianhao626 <1004234098@qq.com>
2023-08-11 23:31:15 +0800

    study-0811

---
+++ test_0811_23_27/test_0811_23_27.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//int main()
+//{
+//    char a = -1;
+//    signed char b = -1;
+//    unsigned char c = -1;
+//    printf("a=%d,b=%d,c=%d", a, b, c);
+//	return 0;
+//}
+
+int main()
+{
+    char a = 128;
+    printf("%u\n", a);
+    return 0;
+}
