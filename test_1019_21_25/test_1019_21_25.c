21f30999fce73792ee465512c8e775cc28dec4dc
bianhao626 <1004234098@qq.com>
2023-10-20 11:43:51 +0800

    study-1020

---
+++ test_1019_21_25/test_1019_21_25.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//Å£¿ÍÍø--BC11
+//int main()
+//{
+//    int a, b, c;
+//    scanf("%d%d%d", &a, &b, &c);
+//    printf("score1=%d,score2=%d,score3=%d", a, b, c);
+//    return 0;
+//}
+
+//Å£¿ÍÍø--BC12
+//typedef struct Student
+//{
+//    char ch[8];
+//    float a;
+//    float b;
+//    float c;
+//}stu;
+//
+//float Change(float x)
+//{
+//    return (x + 0.005) > x ? (x + 0.005) : x;
+//}
+//
+//int main() {
+//    stu S;
+//    scanf("%s%f%f%f", S.ch, &S.a, &S.b, &S.c);
+//    S.a = Change(S.a);
+//    S.b = Change(S.b);
+//    S.c = Change(S.c);
+//    printf("The each subject score of No. %s is %.2f, %.2f, %.2f.",
+//        S.ch, S.a, S.b, S.c);
+//    return 0;
+//}
+
+//int main()
+//{
+//	int id = 0;
+//	float c = 0.0;
+//	float math = 0.0;
+//	float english = 0.0;
+//	scanf("%d;%f,%f,%f", &id, &c, &math, &english);
+//	printf("The each subject score of No. %d is %.2f, %.2f, %.2f.", 
+//		id, c, math, english);
+//	return 0;
+//}
