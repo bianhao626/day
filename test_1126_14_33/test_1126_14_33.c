ca499cb4d999e3a24e75eed4b8c8e216d0a21c5c
bianhao626 <1004234098@qq.com>
2023-11-27 22:08:48 +0800

    study-1127

--- test_1126_14_33/test_1126_14_33.c
+++ test_1126_14_33/test_1126_14_33.c
@@ -38,6 +38,82 @@ TNode* CreatTree()
 	return node1;
 }
 
+
+
+
+TNode* BinaryTreeCreate(TreeDataType* a, int n, int* pi)
+{
+	//前序遍历建树
+	if (a[*pi] == '#' || *pi >= n)
+	{
+		(*pi)++;
+		return NULL;
+	}
+	TNode* root = (TNode*)malloc(sizeof(TNode));
+	root->val = a[*pi++];
+	root->left = BinaryTreeCreate(a, n, pi);
+	root->right = BinaryTreeCreate(a, n, pi);
+}
+void BinaryTreeDestroy(TNode** root)
+{
+	assert(root);
+	if (*root == NULL)
+	{
+		return;
+	}
+	BinaryTreeDestroy(&(*root)->left);
+	BinaryTreeDestroy(&(*root)->right);
+	free(root);
+}
+
+int TreeSize(TNode* node)
+{
+	//方案一，定义全局的变量
+	//方案二
+	return node == NULL ? 0 : TreeSize(node->left) + TreeSize(node->right) + 1;
+}
+
+int TreeLeafSize(TNode* node)
+{
+	if (node == NULL)
+	{
+		return 0;
+	}
+	if (node->left == NULL && node->right == NULL)
+	{
+		return 1;
+	}
+	return TreeLeafSize(node->left) + TreeLeafSize(node->right);
+}
+
+int TreeLevelKSize(TNode* node, int k)
+{
+	assert(k > 0);
+	if (node == NULL)
+	{
+		return 0;
+	}
+	if (k == 1)
+	{
+		return 1;
+	}
+	return TreeLevelKSize(node->left, k - 1) + TreeLevelKSize(node->right, k - 1);
+}
+
+TNode* BinaryTreeFind(TNode* root, TreeDataType x)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	if (root->val == x)
+	{
+		return root;
+	}
+	BinaryTreeFind(root->left, x);
+	BinaryTreeFind(root->right, x);
+}
+
 void PreOrder(TNode* node)
 {
 	//前序递归遍历
@@ -77,39 +153,14 @@ void PostOrder(TNode* node)
 	printf("%d ", node->val);
 }
 
-int TreeSize(TNode* node)
+void BinaryTreeLevelOrder(TNode* node)
 {
-	//方案一，定义全局的变量
-	//方案二
-	return node == NULL ? 0 : TreeSize(node->left) + TreeSize(node->right) + 1;
+
 }
 
-int TreeLeafSize(TNode* node)
-{
-	if (node == NULL)
-	{
-		return 0;
-	}
-	if (node->left == NULL && node->right == NULL)
-	{
-		return 1;
-	}
-	return TreeLeafSize(node->left) + TreeLeafSize(node->right);
-}
 
-int TreeLevelKSize(TNode* node, int k)
-{
-	assert(k > 0);
-	if (node == NULL)
-	{
-		return 0;
-	}
-	if (k == 1)
-	{
-		return 1;
-	}
-	return TreeLevelKSize(node->left, k - 1) + TreeLevelKSize(node->right, k - 1);
-}
+
+
 
 int TreeHight(TNode* node)
 {
@@ -133,9 +184,11 @@ int main()
 	PostOrder(Tree);
 	printf("\n");
 
-	printf("%d\n", TreeSize(Tree));
-	printf("%d\n", TreeLeafSize(Tree));
-	printf("%d\n", TreeHight(Tree));
-	printf("%d\n", TreeLevelKSize(Tree, 2));
+	//printf("%d\n", TreeSize(Tree));
+	//printf("%d\n", TreeLeafSize(Tree));
+	//printf("%d\n", TreeHight(Tree));
+	//printf("%d\n", TreeLevelKSize(Tree, 2));
+	TNode* tmp = BinaryTreeFind(Tree, 4);
+	printf("%d\n", tmp->val);
 	return 0;
 }
